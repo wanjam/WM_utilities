de0ab03c9ede54992bfc355c72bcdb36d9cda036
wanjam <wanjas_github@outlook.com>
2017-05-17 13:40:49 +0200

    nextchange.m takes logicals as input

--- Misc/nextchange.m
+++ Misc/nextchange.m
@@ -3,20 +3,25 @@ function [outIdx] = nextchange(Arr,startIdx)
 % usage : [outIdx] = nextchange(Arr,startIdx)
 % specifically, nextchange returns the index of the first value that
 % differs from the value of startIdx
+% in case there is no further change, it returns end+1
 % Wanja Moessing, moessing@wwu.de
 
 if nargin==1
     startIdx = 1;
 end
 
-if isnumeric(Arr)
+if isnumeric(Arr) || islogical(Arr)
     tmpIdx = find(Arr(startIdx:end)~=Arr(startIdx),1);
 elseif iscell(Arr)
     if ischar([Arr{:}])
         tmpIdx = find(~strcmp(Arr(startIdx:end),Arr(startIdx)),1);
     end
 end
-outIdx = startIdx-1 + tmpIdx;
 
+if ~isempty(tmpIdx)
+    outIdx = startIdx-1 + tmpIdx;
+else
+    outIdx = length(Arr)+1;
+end
 end
 
