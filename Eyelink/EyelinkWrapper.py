46b59b7ac10e54b578b0d525e09abeb161064d8e
wanjam <moessing@wwu.de>
2017-08-08 09:26:32 +0200

    some progress on Eyelinkwrapper.py

--- Eyelink/EyelinkWrapper.py
+++ Eyelink/EyelinkWrapper.py
@@ -1,19 +1,20 @@
 # -*- coding: utf-8 -*-
 """
 WM_EyelinkWrapper.py
-A collection of functions to make interaction with the eyelink 1000+ more
+A custom module to make interaction with the eyelink 1000+ more
 straightforward.
 
-Created on Fri Jul 14 17:46:46 2017
-@author: Wanja Moessing
+First Version July 2017
+__author__ = "Wanja A. Mössing"
+__copyright__ = "Copyright 2017, Institute for Experimental Psychology, WWU Münster, Germany"
+__email__ = "moessing@wwu.de"
 """
 
+#import dependencies to global
+import pylink
 
 def AvoidWrongTriggers():
-    """
-    Throws an error if Eyelink is connected but not
-    configured.
-
+    """ Throws an error if Eyelink is connected but not configured.
     Since we use a Y-cable, that is supposed to send the TTL-triggers from
     the PTB-PC to the EEG and he Eyetracker host, there is a permanent
     connection between the three parallelports (1. EEG, 2.PTB, 3.Eyelink).
@@ -37,7 +38,7 @@ def AvoidWrongTriggers():
 
     if r != 0:
         msg = """Eyelink host PC is turned on. Without running the appropriate
-        startup routines (e.g., `EyelinkStart.m` from Wanja`s Github repo),
+        startup routines (e.g., `EyelinkStart` from Wanja`s Github repo),
         this will cause faulty triggervalues in your EEG signal, because the
         parallel-port of the Eyelink-PC is set to a random value at startup
         instead of reading the port. Either turn off the Eyelink-PC or
@@ -47,7 +48,7 @@ def AvoidWrongTriggers():
 
 
 def EyelinkStart(P, window, Name, inputDialog, FilePreamble):
-    """
+    """ Start connection to EL1000+
     Performs startup routines for the EyeLink 1000 Plus
     eyetracker. 'P' as input should P.myWidth & P.myHeight (= Scrn Resolution),
     P.BgColor (Background color of the experiment), and P.trackr.dummymode
@@ -63,3 +64,42 @@ def EyelinkStart(P, window, Name, inputDialog, FilePreamble):
     NOTE: FilePreamble always needs to start & end with <'''> (3x)
     Creates P.el & P.eye_used
     """
+    # import modules that don't necessarily need to imported to global
+    from os import path, getcwd
+
+    # get filename
+    if '.edf' not in Name:
+        if len(Name) > 8:
+            print('EDF filename too long! (1-8 characters/letters)')
+            raise SystemExit
+        else:
+            Name += '.edf'
+    elif '.edf' in Name:
+        if len(Name) > 12:
+            print('EDF filename too long! (1-8 characters/letters)')
+            raise SystemExit
+
+    # specify file preamble
+    currentdir = path.basename(getcwd())
+    FilePreamble = "''Eyetracking Dataset AE Busch WWU Muenster Experiment: "
+    FilePreamble += currentdir + "''"
+
+    # initialize tracker object
+    eyelinktracker = pylink.EyeLink()
+
+    # initialize graphics. This assumes display initialization.
+    pylink.openGraphics()
+
+    # Open EDF file on host
+    getEyelink().openDataFile(Name)
+
+    # flush all key presses and set tracker mode to offline.
+    pylink.flushGetkeyQueue()
+    getEYELINK().setOfflineMode()
+
+    # Sets the display coordinate system and sends mesage to that
+    # effect to EDF file;
+    getEYELINK().sendCommand("screen_pixel_coords =  0 0 %d %d" %
+                             (P.myWidth - 1, P.myHeight - 1))
+    getEYELINK().sendMessage("DISPLAY_COORDS  0 0 %d %d" %
+                             (P.myWidth - 1, P.myHeight - 1))
---
+++ Eyelink/README.md
@@ -0,0 +1,13 @@
+#Eyelink Wrappers
+A collection of functions to make interacting with the Eyelink 1000+ 
+during the experiment more straight-forward.
+All of these functions require the Eyelink C API included in SR-Research's
+developer pack.
+
+#Matlab functions
+Just add the directory to your path and call the .m-functions from within
+your Psychtoolbox script.
+
+#Python functions
+import the module at the top of your Psychopy/PyGame script.
+Note that this module automatically imports pylink into global namespace.
