7d9a1a6420443a30c5c5d7b67f21b01da901033f
wanjam <moessing@wwu.de>
2017-08-09 11:48:44 +0200

    progress push Eyelinkwrapper

--- Eyelink/EyelinkWrapper.py
+++ Eyelink/EyelinkWrapper.py
@@ -6,12 +6,15 @@ straightforward.
 
 First Version July 2017
 __author__ = "Wanja A. Mössing"
-__copyright__ = "Copyright 2017, Institute for Experimental Psychology, WWU Münster, Germany"
+__copyright__ = "Copyright 2017, Institute for Experimental Psychology,
+    WWU Münster, Germany"
 __email__ = "moessing@wwu.de"
 """
 
-#import dependencies to global
+# import dependencies to global
 import pylink
+from os import path, getcwd, stat, mkdir
+
 
 def AvoidWrongTriggers():
     """ Throws an error if Eyelink is connected but not configured.
@@ -46,37 +49,63 @@ def AvoidWrongTriggers():
         print(msg)
         raise SystemExit
 
-def EyelinkCalibrate(el, dispsize, colors=((0,0,0),(192,192,192))):
-    pylink.setCalibrationColors(colors[0], colors[1]) 	#Sets the calibration target and background color
-    pylink.setTargetSize(int(dispsize[0]/70), int(dispsize[0]/300))	#select best size for calibration target
+
+def EyelinkCalibrate(dispsize, el=pylink.getEYELINK(),
+                     colors=((0, 0, 0), (192, 192, 192))):
+    """ Performs calibration for Eyelink 1000+.
+
+    **Author** : Wanja Mössing, WWU Münster | moessing@wwu.de \n
+    *July 2017*
+
+    Parameters:
+    ----------
+    dispsize : tuple
+        two-item tuple width & height in px
+    el :
+        Eyelink object, optional
+    colors  : Tuple, Optional.
+        Tuple with two RGB triplets
+    """
+    # Sets the calibration target and background color
+    pylink.setCalibrationColors(colors[0], colors[1])
+    # Select best size for calibration target
+    pylink.setTargetSize(int(dispsize[0]/70), int(dispsize[0]/300))
     pylink.setCalibrationSounds("", "", "")
-    pylink.setDriftCorrectSounds("", "off", "off")
+    pylink.setDriftCorrectSounds("", "", "")
     pylink.openGraphics()
     el.doTrackerSetup()
     pylink.closeGraphics()
 
-def EyelinkStart(dispsize, Name, bits=32, dummy=False, colors=((0,0,0),(192,192,192))):
-    """ Start connection to EL1000+
-    dispsize: two-item tuple width & height
-    bits    : color-depth, defaults to 32
-    
-    Performs startup routines for the EyeLink 1000 Plus
-    eyetracker. 'P' as input should P.myWidth & P.myHeight (= Scrn Resolution),
-    P.BgColor (Background color of the experiment), and P.trackr.dummymode
-    (1=there's no real tracker connected).
-    window' refers to the current window pointer
-    Name' is the filename to which data are written. Should end with
-    .edf'. Must be called after first window has been flipped.
-    If inputDialog is 1, you're prompted for a filename, with 'Name' entered
-    as default. Else, Name is simply taken as filename.
-    Can take the optional argument FilePreamble to specify an
-    experiment-specific preamble in the edf-files. Defaults to:
-    Eyetracking Dataset AE Busch WWU Muenster <current working directory>
-    NOTE: FilePreamble always needs to start & end with <'''> (3x)
-    Creates P.el & P.eye_used
+
+def EyelinkStart(dispsize, Name, bits=32, dummy=False,
+                 colors=((0, 0, 0), (192, 192, 192))):
+    """ Performs startup routines for the EyeLink 1000 Plus eyetracker.
+
+    **Author** : Wanja Mössing, WWU Münster | moessing@wwu.de \n
+    *July 2017*
+
+    Parameters:
+    -----------
+    dispsize : tuple
+        two-item tuple width & height in px
+    Name    : string
+        filename for the edf. Doesn't have to, but can, end on '.edf'
+        Maximum length is 8 (without '.edf').
+        Possible alphanumeric input: 'a-z', 'A-Z', '0-9', '-' & '_'
+    bits    : integer
+        color-depth, defaults to 32
+    dummy   : boolean
+        Run tracker in dummy mode?
+    colors  : Tuple, Optional.
+        Tuple with two RGB triplets
+
+    Returns
+    -------
+    'el' the tracker object.
+             This can be passed to other functions,
+             although they can use pylink.getEYELINK()
+             to find it automatically.
     """
-    # import modules that don't necessarily need to imported to global
-    from os import path, getcwd
 
     # get filename
     if '.edf' not in Name.lower():
@@ -98,21 +127,19 @@ def EyelinkStart(dispsize, Name, bits=32, dummy=False, colors=((0,0,0),(192,192,
 
     # initiate graphics
     pylink.openGraphics(dispsize, bits)
-    
+
     # Open EDF file on host
     el.openDataFile(Name)
-    
-    #flush old keys
+
+    # flush old keys
     pylink.flushGetkeyQueue()
 
     # Sets the display coordinate system and sends mesage to that
     # effect to EDF file;
     el.sendCommand("screen_pixel_coords =  0 0 %d %d" %
-                             (dispsize[0] - 1, dispsize[1] - 1))
+                   (dispsize[0] - 1, dispsize[1] - 1))
     el.sendMessage("DISPLAY_COORDS  0 0 %d %d" %
-                             (dispsize[0] - 1, dispsize[1] - 1))
-
-
+                   (dispsize[0] - 1, dispsize[1] - 1))
 
     # select parser configuration for online saccade etc detection
     ELversion = el.getTrackerVersion()
@@ -121,43 +148,128 @@ def EyelinkStart(dispsize, Name, bits=32, dummy=False, colors=((0,0,0),(192,192,
         tmp = el.getTrackerVersionString()
         tmpidx = tmp.find('EYELINK CL')
         ELsoftVer = int(float(tmp[(tmpidx + len("EYELINK CL")):].strip()))
-    if ELversion>=2:
+    if ELversion >= 2:
         el.sendCommand("select_parser_configuration 0")
-    if ELversion==2:
+    if ELversion == 2:
         # turn off scenelink stuff (that's an EL2 front-cam addon...)
         el.sendCommand("scene_camera_gazemap = NO")
     else:
         el.sendCommand("saccade_velocity_threshold = 35")
         el.sendCommand("saccade_acceleration_threshold = 9500")
-	    
-	
-    # set EDF file contents 
-    el.sendCommand("file_event_filter = LEFT,RIGHT,FIXATION,SACCADE,AREA,BLINK,MESSAGE,BUTTON,INPUT")
-    if ELsoftVer>=4:
-	    el.sendCommand("file_sample_data  = LEFT,RIGHT,GAZE,HREF,AREA,HTARGET,GAZERES,STATUS,INPUT")
-    else:
-        el.sendCommand("file_sample_data  = LEFT,RIGHT,GAZE,HREF,AREA,GAZERES,STATUS,INPUT")
 
-    # set link data (online interaction) 
-    el.sendCommand("link_event_filter = LEFT,RIGHT,FIXATION,SACCADE,AREA,BLINK,MESSAGE,BUTTON,INPUT")
-    if ELsoftVer>=4:
-	    el.sendCommand("link_sample_data  = LEFT,RIGHT,GAZE,GAZERES,AREA,HTARGET,STATUS,INPUT")
+    # set EDF file contents
+    el.sendCommand("file_event_filter = LEFT,RIGHT,FIXATION,"
+                   "SACCADE,AREA,BLINK,MESSAGE,BUTTON,INPUT")
+    if ELsoftVer >= 4:
+        el.sendCommand("file_sample_data = LEFT,RIGHT,GAZE,HREF,"
+                       "AREA,HTARGET,GAZERES,STATUS,INPUT")
     else:
-        el.sendCommand("link_sample_data  = LEFT,RIGHT,GAZE,GAZERES,AREA,STATUS,INPUT")
-        
-    #set file preamble
+        el.sendCommand("file_sample_data = LEFT,RIGHT,GAZE,HREF,"
+                       "AREA,GAZERES,STATUS,INPUT")
+
+    # set link data (online interaction)
+    el.sendCommand("link_event_filter = LEFT,RIGHT,FIXATION,SACCADE,"
+                   "AREA,BLINK,MESSAGE,BUTTON,INPUT")
+    if ELsoftVer >= 4:
+        el.sendCommand("link_sample_data = LEFT,RIGHT,GAZE,GAZERES,AREA,"
+                       "HTARGET,STATUS,INPUT")
+    else:
+        el.sendCommand("link_sample_data = LEFT,RIGHT,GAZE,GAZERES,AREA,"
+                       "STATUS,INPUT")
+
+    # set file preamble
     currentdir = path.basename(getcwd())
     FilePreamble = "''Eyetracking Dataset AE Busch WWU Muenster Experiment: "
     FilePreamble += currentdir + "''"
     el.sendcommand("add_file_preamble_text " + FilePreamble)
-    
+
+    # run initial calibration
+    # 13-Pt Grid calibration
+    el.sendCommand('calibration_type = HV13')
     EyelinkCalibrate(el, dispsize, colors)
-    
-    #put tracker in idle mode and wait 50ms, then really start it.
+
+    # put tracker in idle mode and wait 50ms, then really start it.
     el.sendMessage('SETUP_FINISHED')
     el.sendCommand('set_idle_mode')
     el.sendCommand('clear_screen 0')
     pylink.msecDelay(500)
-    
-    #start recording
+
+    # start recording
     el.startRecording()
+
+    # to activate parallel port readout without modifying the FINAL.INI on the
+    # eyelink host pc, uncomment these lines
+    # tyical settings for straight-through TTL cable (data pins -> data pins)
+    el.sendCommand('write_ioport 0xA 0x20')
+    el.sendCommand('create_button 1 8 0x01 0')
+    el.sendCommand('create_button 2 8 0x02 0')
+    el.sendCommand('create_button 3 8 0x04 0')
+    el.sendCommand('create_button 4 8 0x08 0')
+    el.sendCommand('create_button 5 8 0x10 0')
+    el.sendCommand('create_button 6 8 0x20 0')
+    el.sendCommand('create_button 7 8 0x40 0')
+    el.sendCommand('create_button 8 8 0x80 0')
+    el.sendCommand('input_data_ports  = 8')
+    el.sendCommand('input_data_masks = 0xFF')
+    # tyical settings for crossover TTL cable (data pins -> status pins)
+#    el.sendCommand('write_ioport 0xA 0x0')
+#    el.sendCommand('create_button 1 9 0x20 1')
+#    el.sendCommand('create_button 2 9 0x40 1')
+#    el.sendCommand('create_button 3 9 0x08 1')
+#    el.sendCommand('create_button 4 9 0x10 1')
+#    el.sendCommand('create_button 5 9 0x80 0')
+#    el.sendCommand('input_data_ports  = 9')
+#    el.sendCommand('input_data_masks = 0xFF')
+
+    # mark end of Eyelinkstart in .edf
+    el.sendMessage('>EndOfEyeLinkStart')
+    # return Eyelink object
+    return(el)
+
+
+def EyelinkStop(el=pylink.getEYELINK()):
+    """ Performs stopping routines for the EyeLink 1000 Plus eyetracker.
+
+    **Author** : Wanja Mössing, WWU Münster | moessing@wwu.de \n
+    *July 2017*
+
+    Parameters:
+    -----------
+    dispsize : tuple
+        two-item tuple width & height in px
+    Name    : string
+        filename for the edf. Doesn't have to, but can, end on '.edf'
+        Maximum length is 8 (without '.edf').
+        Possible alphanumeric input: 'a-z', 'A-Z', '0-9', '-' & '_'
+    bits    : integer
+        color-depth, defaults to 32
+    dummy   : boolean
+        Run tracker in dummy mode?
+    colors  : Tuple, Optional.
+        Tuple with two RGB triplets
+
+    Returns
+    -------
+    'el' the tracker object.
+             This can be passed to other functions,
+             although they can use pylink.getEYELINK()
+             to find it automatically.
+    """
+    # make sure all experimental procedures finished
+    pylink.msecDelay(1000)
+    # stop the recording
+    el.stopRecording()
+    # put Eyelink back to idle
+    el.sendCommand('set_idle_mode')
+    #
+    # wait for stuff to finish
+    pylink.msecDelay(500)
+    # close edf
+    el.closeDataFile
+    # transfer edf to display-computer
+    try:
+        disp('Wait for EDF to be copied over LAN...')
+        if not os.path.exists('./EDF'):
+            os.mkdir('./EDF')
+        
+        
