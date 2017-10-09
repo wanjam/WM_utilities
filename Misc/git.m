4a3136e2a717edcb5deed38b1f925693905c9014
wanjam <w.a.m@wwu.de>
2017-10-09 14:38:48 +0200

    Allow complex input for git.m

--- Misc/git.m
+++ Misc/git.m
@@ -1,7 +1,12 @@
-function git(cmd)
+function git(varargin)
 %GIT simply wraps the git cmd for matlab
 %
 % Wanja Moessing, October 2017, moessing@wwu.de
+if nargin>1
+    cmd = strjoin(varargin,' ');
+else
+    cmd = varargin{:};
+end
 system(sprintf('git %s', cmd));
 end
 
