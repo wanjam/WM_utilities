6e3b0fe0d52e9d3274261bfd74143a29ed357f35
Wanja Mössing <moessing@wwu.de>
2018-03-13 18:10:25 +0100

    Add simple flac2mp3
    - converts all flac audiofiles found in the current folder
    - output is 128kb mp3
    - needs `conda install ffmpeg`

---
+++ Misc/flac2mp3.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python
+
+from pydub import AudioSegment
+from pydub.utils import mediainfo
+from glob import glob
+from os import path
+from warnings import warn
+
+# get all flacs in current folder
+flac_files = glob('./*.flac')
+
+
+for i in range(0, len(flac_files)):
+    cur_flac = AudioSegment.from_file(flac_files[i], 'flac')
+    outfile = path.splitext(flac_files[i])[0] + '.mp3'
+    
+    if path.isfile(outfile):
+        warn('File ' + outfile + ' already exists. Skipping...')
+    else:
+        id3tags = mediainfo(flac_files[i]).get('TAG', {})
+        cur_flac.export(outfile, format = 'mp3', tags = id3tags, bitrate = '128k')
