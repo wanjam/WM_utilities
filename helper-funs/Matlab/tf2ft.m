8c5c48b25ae357294705f954340eba748b14e141
Wanja Mössing <moessing@wwu.de>
2018-04-30 18:17:51 +0200

    tf2ft: add single-trial case

--- helper-funs/Matlab/tf2ft.m
+++ helper-funs/Matlab/tf2ft.m
@@ -1,10 +1,16 @@
-function [FT] = tf2ft(TF, data_chans, datafieldname)
+function [FT] = tf2ft(TF, varargin)
 %TF2FT Prepares the output of design_runtf for fieldtrip
 %   This function takes a TF-Structure as is produced by Elektro-Pipe's
 %   design_runtf and converts the data to a format that fieldtrip's
 %   permutation testing functions can digest.
 %   Crucially, it assumes TF.pow is:
 %           4D: freqs x times x channels x subject.
+%   OR
+%           4D: freqs x times x trials x channels.
+%
+%   Optional input 'singletrial' can be true | false (default). If true, data of a
+%   single subject are transformed at a single-trial level. This is useful
+%   for, e.g., cosmomvpa.
 %
 %   Optional input 'data_chans' can be a vector defining which channels in
 %   TF.chanlocs are data channels. Usually this can be CFG.data_chans. If
@@ -29,7 +35,7 @@ function [FT] = tf2ft(TF, data_chans, datafieldname)
 %   Nchan*1 cell with all DATA-channels (CFG.data_chans)
 %
 % time:
-%   1*Ntrial cell with each cell (1*NTimepoints) vector containing human 
+%   1*Ntrial cell with each cell (1*NTimepoints) vector containing human
 %   readable time in seconds.
 %
 % trial:
@@ -38,42 +44,86 @@ function [FT] = tf2ft(TF, data_chans, datafieldname)
 % fsample:
 %   the current sampling rate
 
-% input checks
-if nargin < 3
-    datafieldname = 'pow';
+
+%% input checks
+p = inputParser;
+p.FunctionName = 'tf2ft';
+p.addRequired('TF',@isstruct);
+p.addOptional('singletrial', false, @islogical);
+p.addOptional('data_chans', 0);
+p.addOptional('datafieldname', 'pow', @isstr);
+parse(p, TF, varargin{:})
+
+datafieldname = p.Results.datafieldname;
+data_chans    = p.Results.data_chans;
+singletrial   = p.Results.singletrial;
+
+
+%% Transform input
+switch singletrial
+    case false  % avergage case
+        
+        if data_chans == 0
+            data_chans = 1:size(TF.(datafieldname), 3);
+        end
+        % header
+        FT.hdr.Fs = TF.old_srate;
+        FT.hdr.nChans = length({TF.chanlocs.labels});
+        FT.hdr.nSamples = length(TF.times);
+        FT.hdr.nSamplesPre = sum(TF.times<0);
+        FT.hdr.nTrials = length(TF.trials);
+        FT.hdr.label = {TF.chanlocs.labels}';
+        
+        % channel info
+        FT.label = FT.hdr.label(data_chans);
+        FT.elec.label = FT.label;
+        CH = TF.chanlocs(data_chans);
+        FT.elec.pnt   = [[CH.X]', [CH.Y]', [CH.Z]'];
+        FT.eeglabChanlocs = CH;
+        clear CH
+        
+        % data info
+        FT.freq = TF.freqs;
+        FT.time = TF.times;
+        FT.dimord = 'subj_chan_freq_time';
+        FT.fsample = TF.new_srate;
+        
+        % data
+        FT.powspctrm = permute(TF.(datafieldname), [4, 3, 1, 2]);
+        
+    case true  % singletrial case
+        
+        if data_chans == 0
+            data_chans = 1:size(TF.single.(datafieldname), 4);
+        end
+        
+        % header
+        FT.hdr.Fs = TF.old_srate;
+        FT.hdr.nChans = length({TF.single.chanlocs.labels});
+        FT.hdr.nSamples = length(TF.single.times);
+        FT.hdr.nSamplesPre = sum(TF.single.times < 0);
+        FT.hdr.nTrials = size(TF.single.(datafieldname), 3);
+        FT.hdr.label = {TF.single.chanlocs.labels}';
+        
+        % channel info
+        FT.label = FT.hdr.label(data_chans);
+        FT.elec.label = FT.label;
+        CH = TF.single.chanlocs(data_chans);
+        FT.elec.pnt   = [[CH.X]', [CH.Y]', [CH.Z]'];
+        FT.eeglabChanlocs = CH;
+        clear CH
+        
+        % data info
+        FT.freq = TF.single.freqs;
+        FT.time = TF.single.times;
+        FT.dimord = 'trial_chan_freq_time';
+        FT.fsample = TF.new_srate;
+        
+        % is this necessary?
+        %[FT.time{1:length(TF.trials)}] = deal(TF.times);
+        
+        % data
+        FT.powspctrm = permute(TF.single.(datafieldname), [3, 4, 1, 2]);
 end
 
-if nargin < 2
-    data_chans = 1:size(TF.(datafieldname), 3);
 end
-
-% header
-FT.hdr.Fs = TF.old_srate;
-FT.hdr.nChans = length({TF.chanlocs.labels});
-FT.hdr.nSamples = length(TF.times);
-FT.hdr.nSamplesPre = sum(TF.times<0);
-FT.hdr.nTrials = length(TF.trials);
-FT.hdr.label = {TF.chanlocs.labels}';
-
-% channel info
-FT.label = FT.hdr.label(data_chans);
-FT.elec.label = FT.label;
-CH = TF.chanlocs(data_chans);
-FT.elec.pnt   = [[CH.X]', [CH.Y]', [CH.Z]'];
-FT.eeglabChanlocs = CH;
-clear CH
-
-% data info
-FT.freq = TF.freqs;
-FT.time = TF.times;
-FT.dimord = 'subj_chan_freq_time';
-FT.fsample = TF.new_srate;
-
-% here could be code for the single trial case
-%[FT.time{1:length(TF.trials)}] = deal(TF.times);
-
-% data
-FT.powspctrm = permute(TF.(datafieldname), [4, 3, 1, 2]);
-
-end
-
