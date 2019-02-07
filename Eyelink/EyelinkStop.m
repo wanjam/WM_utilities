5e1777d78c37cda9c57884c79857d19a57361384
Wanja Mössing <wanjam@mailbox.org>
2019-02-07 11:29:20 +0100

    Update EyelinkStop

--- Eyelink/EyelinkStop.m
+++ Eyelink/EyelinkStop.m
@@ -1,14 +1,23 @@
-function EyelinkStop(P, eyelinkconnected)
-% EYELINKSTOP performs the usual stopping routines for the Eyelink 1000 plus
-% eyetracker. Needs P.trackr.edfFile, which should have been created with a
-% previous call to EYELINKSTART
+function EyelinkStop(P, eyelinkconnected, outfolder)
+% EYELINKSTOP performs the usual stopping routines for the Eyelink 1000+
+% eyetracker. 
 %
-% eyelinkconnected should be 0 or 1 (default). If 0, the function does
-% nothing.
+% INPUT:
+%       'P': Structure with at least field P.trackr.edfFile, which should 
+%            have been created with a previous call to EYELINKSTART
+%       'eyelinkconnected': logical, defaults to true. If 0, the function
+%                           is a dummy. Useful for testing.
+%       'outfolder': String. Path where EDFs should be stored. Defaults to
+%                    './Data/EyeData'. Folder is created on-the-fly.
+%
+%       'P.EDFdir': alternative way to specify outfolder
+%       'P.isET': alternative way to specify eyelinkconnected
+%
 %
 % Wanja Moessing 28/02/2016
+% Wanja Moessing 07/02/2019: Implement Elio Balestrieri's suggestions
 
-%  Copyright (C) 2016 Wanja MÃ¶ssing
+%  Copyright (C) 2016-2019 Wanja Mössing
 %
 %  This program is free software: you can redistribute it and/or modify
 %  it under the terms of the GNU General Public License as published by
@@ -23,9 +32,26 @@ function EyelinkStop(P, eyelinkconnected)
 %  You should have received a copy of the GNU General Public License
 %  along with this program. If not, see <http://www.gnu.org/licenses/>.
 
+% check if output folder is specified
+if nargin < 3 & ~isfield(P, 'EDFdir')
+    outdir = ['.', filesep,'Data', filesep,'EyeData', filesep];
+elseif nargin > 2
+    outdir = outfolder;
+elseif isfield(P, 'EDFdir')
+    try
+        [~, ~] = mkdir(P.trackr.out);
+    catch
+        warning(['EyelinkStop: Couldn''t create indicated ',...
+            'folder. Will use ''./Data/EyeData/'' instead...']);
+        outdir = ['.', filesep,'Data', filesep,'EyeData', filesep];
+    end
+end
+
 % check if eyelink is not connected
-if nargin < 2
+if nargin < 2 & ~isfield(P, 'isET')
     eyelinkconnected = 1;
+elseif isfield(P, 'isET')
+    eyelinkconnected = P.isET;
 end
 if eyelinkconnected == 0
     disp('No Eyelink connected. No connection to close...');
@@ -50,17 +76,17 @@ if P.trackr.dummymode == 0
     
     % download data file
     try
-        fprintf('Receiving Eyetracker data file ''%s''\n',...
-            P.trackr.edfFile);
-        [~,~,~] = mkdir('./Data', 'EyeData');
-        status=Eyelink('ReceiveFile', P.trackr.edfFile,...
-            './Data/EyeData/', 1);
+        fprintf('Receiving Eyetrack data file ''%s''\n', P.trackr.edfFile);
+        [~, ~] = mkdir(outdir);
+        status = Eyelink('ReceiveFile', P.trackr.edfFile, outdir, 1);
         if status > 0
             fprintf('ReceiveFile status %d\n', status);
         end
-        if 2==exist(P.trackr.edfFile, 'file')
+        if 2 == exist(P.trackr.edfFile, 'file')
+            foo = dir(outdir);
+            foo = foo(1).folder;
             fprintf('Data file ''%s'' can be found in ''%s''\n',...
-                P.trackr.edfFile, pwd );
+                P.trackr.edfFile, foo);
         end
     catch
         try
