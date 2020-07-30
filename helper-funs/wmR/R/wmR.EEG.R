f8bd6e75f5609e408879845cd2acece90721d8d1
Wanja <moessing@wwu.de>
2020-07-30 14:40:46 +0200

    Add Matlab/EEGLAB --> R/EEGutils interface

--- helper-funs/wmR/R/wmR.EEG.R
+++ helper-funs/wmR/R/wmR.EEG.R
@@ -162,3 +162,190 @@ TF_ggplot <- function(TF, zlim, do_interpolate = T, contours = F,
   }
   return(plt)
 }
+
+
+
+##' @title Importing eeg tables from EEGlab
+##' @description This is the only way to import eeglab data to eegUtils that
+##' really worked for me. This assumes that you wrangled your data in eeglab/matlab
+##' into a long table and then exported it as a csv. The csv should have the
+##' following format:
+##'\itemize{
+##' \item one columns per Electrode, column names should be electrode names.
+##' \item one column coding participant ID, column name must be 'participant_id' (can also be used for trials)
+##' \item one column coding time, called 'time'
+##' \item one column with a unique value representing sampling rate, called 'srate'
+##' \item one column with a unique value representing the used reference, called 'reference'
+##'}
+##' How to get your data into that format likely depends heavily on the specific
+##'  situation. Thus, there's no matlab-pendant to this function. Nevertheless,
+##' in the examples section below, you can find one possible way of accomplishing
+##'  this in Matlab.
+##'
+##' Note that this function can currently read neither the EEG.events structure
+##' (good luck flattening that to a csv...), nor the EEG.chanlocs structure.
+##' Chanlocs are substituted by reading the channel locations from a .txt file
+##' instead.
+##'
+##'
+##' Note: depends on Matt Craddock's eegUtils package, which is not available on
+##' CRAN. Use \code{remotes::install_github("craddm/eegUtils")} instead.
+##'
+##'
+##' @param DT a \code{data.table} with exactly the columns specified for the csv above
+##' @param channel_locfile a .txt file with spherical coordinates for your channels.
+##' defaults to the scalp-channels of the custom Easycap montage used in our lab
+##' at the WWU Münster, Germany.
+##' @return an \code{eegUtils} object
+##' @examples
+##' # Matlab first, R below
+##' \dotrun{
+##' #%%%%% EXPORTING IN MATLAB %%%%% (usage in R below)
+##' # EEG.bsldata are baselined data (64x1900x30; chan x time x id)
+##' # stack the third dimension, so this becomes 2D
+##' #FLAT = reshape(EEG.bsldata, size(EEG.bsldata,1),[],1);
+##' #
+##' #% make this a table
+##' #FLAT = array2table(FLAT');
+##' #
+##' #% set column names to channel names
+##' #CHANS = {EEG.chanlocs.labels};
+##' #FLAT.Properties.VariableNames = CHANS;
+##' #
+##' #% Add column that codes ID
+##' #ID = repelem(1:size(EEG.bsldata, 3), size(EEG.bsldata, 2))';
+##' #                   FLAT.participant_id = ID;
+##' #
+##' #% add column that codes time
+##' #TIME = repmat(EEG.times, 1, size(EEG.bsldata,3))';
+##' #FLAT.time = TIME;
+##' #
+##' #% add column with unique sampling rate value
+##' #FLAT.srate = repelem(EEG.srate, height(FLAT), 1);
+##' #
+##' #% add column with unique reference name
+##' #FLAT.reference = repelem('average', height(FLAT), 1);
+##' #
+##' #% write table to csv
+##' #writetable(FLAT, 'foo.csv')
+##' #
+##' #-----------------------------
+##' #-----------------------------
+##' #-----------------------------
+##' ###### Importing in R #######
+##' wmR::libraries(data.table, eegUtils, ggplot2, wmR)
+##' EEG <- fread('foo.csv')
+##' EEG <- EEG = eeglabCSV2eegUtils(EEG)
+##' subEEG = select_times(EEG, c(0,100))
+##' ggplot(subEEG  , aes(x = x, y = y, fill = amplitude)) + geom_topo()
+##' }
+##' @author Wanja Mössing
+##' @name eeglabCSV2eegUtils
+##' @seealso eegUtils txt_elec_2_eegUtils
+##' @import data.table eegUtils assertthat
+##' @export eeglabCSV2eegUtils
+##' @importFrom data.table data.table
+eeglabCSV2eegUtils <- function(DT, channel_locfile = NULL) {
+  # create artifical sample info (needs to be an ongoing counter)
+  DT[, sample := .I]
+
+  # now act as if participants were epochs
+  DT[, epoch := participant_id]
+  assert_that(DT[, is.numeric(participant_id)],
+              msg = 'please use numbers for `participant_id`.')
+  EPOCH = dplyr::tibble(
+    DT[, .(participant_id = '001', recording = NA, epoch_label = NA),
+             by = epoch]
+    )
+
+  # get sampling rate
+  srate <- DT[, unique(srate)]
+  assert_that(length(srate) == 1, msg = 'srate not uniform. please read ?eeglabCSV2eegUtils')
+
+  # get chanlocs
+  chanlocs <- wmR::txt_elec_2_eegUtils(channel_locfile)
+
+  # reduce dataset to eeg channels
+  CHANS = colnames(DT)[!(colnames(DT) %like% 'participant_id|time|srate|sample|epoch|reference')]
+  DATA = as.data.frame(DT[,.SD, .SDcols = CHANS])
+
+  # construct timings
+  TIMINGS = dplyr::tibble(DT[, .(epoch = as.double(epoch),
+                                 sample = as.double(sample), time) ])
+
+  # construct reference info
+  REF <- list(ref_chans = DT[, unique(reference)],
+              excluded = NULL)
+
+
+  # create the eeg_data object
+  EEG = eegUtils:::eeg_data(data = DATA, srate = srate, chan_info = chanlocs,
+                            timings = TIMINGS,
+                            epochs = EPOCH,
+                            reference = REF
+                            )
+
+EEG = eegUtils:::validate_eeg_epochs(EEG)
+return(EEG)
+# - might have to fake event structure?
+}
+
+
+##' @title txt_elec_2_eegUtils
+##' @description This is adjusted from \code{eegUtils:::import_chans}, which did
+##' not work for me. Only tested this function with our custom layout file with
+##' the specific constellation used in our lab at the WWU.
+##'
+##' Note: depends on Matt Craddock's eegUtils package, which is not available on
+##' CRAN. Use \code{remotes::install_github("craddm/eegUtils")} instead.
+##'
+##' If you need your own chanlocs, check the necessary format in the file:
+##' \code{system.file("extdata","easycap_m43v3_biosemi_buschlab_scalponly.txt", package = "wmR")}
+##' @param file_name path+name to the .txt file with spherical coordinates. Default
+##' is our custom Easycap montage (Buschlab WWU Münster).
+##' @return a \code{eegUtils} channel location tibble
+##' @examples
+##' chanlocs <- txt_elec_2_eegUtils()
+##' @author Wanja Mössing
+##' @name txt_elec_2_eegUtils
+##' @seealso eegUtils eeglabCSV2eegUtils
+##' @import data.table eegUtils assertthat
+##' @export txt_elec_2_eegUtils
+##' @importFrom data.table data.table
+txt_elec_2_eegUtils <- function(file_name = NULL) {
+  if (is.null(file_name)) {
+    file_name = system.file("extdata",
+                            "easycap_m43v3_biosemi_buschlab_scalponly.txt",
+                            package = "wmR")
+    cat('txt_elec_2_eegUtils: no user input, using default easycap_m43v3_biosemi_buschlab_scalponly layout')
+  }
+  raw_locs = data.table::fread(file_name)
+
+  # deal with naming differences
+  setnames(raw_locs, 'Theta', 'theta', skip_absent = T)
+  setnames(raw_locs, 'Phi', 'phi', skip_absent = T)
+  setnames(raw_locs, new = rep('electrode', 7), skip_absent = T,
+           old = c('Name', 'name', 'Names', 'names', 'electrodes',
+                       'Electrode', 'Electrodes'))
+
+  # phi & theta are supposed to be double, not integer (breaks topoplots?!)
+  # raw_locs[, ':='(phi = as.double(phi), theta = as.double(phi))]
+
+
+  cart_xyz = eegUtils:::sph_to_cart(raw_locs[, theta], raw_locs[, phi])
+
+  final_locs = tibble::tibble(
+    raw_locs[, .(electrode = electrode, theta, phi)][, radius := 1]
+    )
+
+  xy <- eegUtils:::project_elecs(final_locs, method = "stereographic")
+  final_locs = cbind(final_locs, cart_xyz, xy)
+  final_locs = tibble::as_tibble(final_locs)
+
+  # fix order of columns to be consistent with demo dataset in eegUtils pack
+  final_locs = dplyr::relocate(final_locs, electrode, radius, theta, phi,
+                               cart_x, cart_y, cart_z, x, y)
+
+
+  return(final_locs)
+}
